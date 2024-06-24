cd601056da8b2ec69187ce41582140b6e15acb24
Azrotho <azrotho.yt@gmail.com>
2024-06-24 14:45:53 +0200

    first commit

---
+++ src/main/java/fr/azrotho/azmod/AzMod.java
@@ -0,0 +1,16 @@
+package fr.azrotho.azmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AzMod implements ModInitializer {
+	public static final String MODID = "azmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
