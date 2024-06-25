6a3f9299f61a06493d4ccce13d8bfe69926e237d
Azrotho <azrotho.yt@gmail.com>
2024-06-25 16:44:38 +0200

    rompich vectors :')

--- src/main/java/fr/azrotho/azmod/entity/custom/HookProjectileEntity.java
+++ src/main/java/fr/azrotho/azmod/entity/custom/HookProjectileEntity.java
@@ -8,11 +8,15 @@ import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.text.Text;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.world.World;
 
 public class HookProjectileEntity extends ThrownItemEntity {
+
+    int tick = -1;
 
     public HookProjectileEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
         super(entityType, world);
@@ -33,12 +37,32 @@ public class HookProjectileEntity extends ThrownItemEntity {
     }
 
     @Override
-    protected void onBlockHit(BlockHitResult blockHitResult) {
-        if(!this.getWorld().isClient()) {
-            this.getOwner().teleport(blockHitResult.getBlockPos().getX(), blockHitResult.getBlockPos().getY() + 1, blockHitResult.getBlockPos().getZ());
-            this.getWorld().sendEntityStatus(this, (byte) 3);
+    public void tick() {
+        super.tick();
+        if(tick == -1) {
+            return;
         }
-        this.discard();
+
+        tick--;
+
+        // Propulser le joueur vers la tête du grappin
+        Vec3d velocityToPlayer = this.getOwner().getPos().subtract(this.getPos()).normalize().multiply(0.5);
+        this.getOwner().setVelocity(velocityToPlayer.multiply(100));
+        if(tick == 0) {
+            this.discard();
+        }
+    }
+
+    @Override
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        if(!this.getWorld().isClient() && !this.getCustomName().equals("hited")) {
+            //this.getOwner().teleport(blockHitResult.getBlockPos().getX(), blockHitResult.getBlockPos().getY() + 1, blockHitResult.getBlockPos().getZ());
+            this.setVelocity(0, 0, 0);
+            this.setNoGravity(true);
+            this.setCustomName(Text.of("hited"));
+            this.tick = 100;
+        }
+        //this.discard();
         super.onBlockHit(blockHitResult);
     }
 }
--- src/main/java/fr/azrotho/azmod/item/custom/GrapplingHookItem.java
+++ src/main/java/fr/azrotho/azmod/item/custom/GrapplingHookItem.java
@@ -1,11 +1,14 @@
 package fr.azrotho.azmod.item.custom;
 
 import fr.azrotho.azmod.entity.custom.HookProjectileEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -21,15 +24,31 @@ public class GrapplingHookItem extends Item {
         ItemStack itemStack = player.getStackInHand(hand);
         world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.random.nextFloat() * 0.4F + 0.8F));
 
-        if (!world.isClient) {
+        //if (!world.isClient) {
             HookProjectileEntity hook = new HookProjectileEntity(player, world);
             hook.setItem(itemStack);
+            hook.setCustomName(Text.of("hook"));
             hook.setVelocity(player, player.getPitch(), player.getYaw(), 0.0F, 1.5F, 1.0F);
             world.spawnEntity(hook);
-        }
+            attachEntityToPlayer(hook, player);
+        //}
 
         itemStack.damage(1, player, (p) -> p.sendToolBreakStatus(hand));
 
         return TypedActionResult.success(player.getStackInHand(hand));
+    }
+
+    public static void attachEntityToPlayer(HookProjectileEntity entity, PlayerEntity player) {
+        World world = entity.getWorld();
+
+        // Créer une canne à pêche factice
+        FishingBobberEntity fishingBobber = new FishingBobberEntity(EntityType.FISHING_BOBBER, world);
+        world.spawnEntity(fishingBobber);
+
+        // Lier l'entité à la canne à pêche
+        fishingBobber.startRiding(entity);
+
+        // Lier la canne à pêche au joueur
+        fishingBobber.setOwner(player);
     }
 }
